0eea19af7c1cb304e0c2333209e4e1edae80dc8e
Keith Babinec <keith.babinec@gmail.com>
2019-07-07 20:48:51 -0700

    Added Send-AppInsightsTraceTelemetry function

--- Functions/Public/Send-AppInsightsTraceTelemetry.ps1
+++ Functions/Public/Send-AppInsightsTraceTelemetry.ps1
@@ -1,30 +1,33 @@
-function Send-AppInsightsExceptionTelemetry
+function Send-AppInsightsTraceTelemetry
 {
     <#
     .SYNOPSIS
-        Sends exception telemetry to an Azure Application Insights instance.
+        Sends trace telemetry to an Azure Application Insights instance.
 
     .DESCRIPTION
-        Sends exception telemetry to an Azure Application Insights instance. This function uses the Azure Application Insights REST API instead of a compiled client library, so it works without additional dependencies.
+        Sends trace telemetry to an Azure Application Insights instance. This function uses the Azure Application Insights REST API instead of a compiled client library, so it works without additional dependencies.
 
 		NOTE: Telemetry ingestion to Azure Application Insights typically has a ~2-3 minute delay due to the eventual-consistency nature of the service.
 
     .PARAMETER InstrumentationKey
         Specify the instrumentation key of your Azure Application Insights instance. This determines where the data ends up.
 
-    .PARAMETER Exception
-        Specify the actual exception object to send.
+    .PARAMETER Message
+        Specify the message to log.
+
+    .PARAMETER Severity
+        Specify the message severity. Acceptable values are Verbose, Information, Warning, Error, and Critical.
 
     .PARAMETER CustomProperties
         Optionally specify additional custom properties, in the form of a hashtable (key-value pairs) that should be logged with this telemetry.
 
     .EXAMPLE
-        C:\> Send-AppInsightsEventTelemetry -InstrumentationKey <guid> -Exception $Error[0].Exception
-        Sends exception telemetry to application insights for the most recently logged PowerShell Error.
+        C:\> Send-AppInsightsTraceTelemetry -InstrumentationKey <guid> -Message 'This is an informational logging message.' -Severity Information
+        Sends trace telemetry to application insights.
 
 	.EXAMPLE
-        C:\> Send-AppInsightsEventTelemetry -InstrumentationKey <guid> -Exception $Error[0].Exception -CustomProperties @{ 'CustomProperty1'='abc'; 'CustomProperty2'='xyz' }
-        Sends exception telemetry to application insights for the most recently logged PowerShell Error, with additional custom properties.
+        C:\> Send-AppInsightsTraceTelemetry -InstrumentationKey <guid> -Message 'This is a warning logging message.' -Severity Warning -CustomProperties @{ 'CustomProperty1'='abc'; 'CustomProperty2'='xyz' }
+        Sends trace telemetry to application insights, with additional custom properties tied to this event.
     #>
     [CmdletBinding()]
     Param
@@ -38,10 +41,17 @@ function Send-AppInsightsExceptionTelemetry
 
 		[Parameter(
             Mandatory=$true,
-            HelpMessage='Specify the exception object to send. This should be an actual Exception class and not a PowerShell ErrorRecord.')]
-		[System.Exception]
-		[ValidateNotNull()]
-		$Exception,
+            HelpMessage='Specify the message to log.')]
+		[System.String]
+		[ValidateNotNullOrEmpty()]
+		$Message,
+
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage='Specify the message severity. Acceptable values are Verbose, Information, Warning, Error, and Critical.')]
+		[System.String]
+		[ValidateSet('Verbose','Information','Warning','Error','Critical')]
+		$Severity,
 
 		[Parameter(Mandatory=$false)]
 		[Hashtable]
@@ -66,17 +76,12 @@ function Send-AppInsightsExceptionTelemetry
 			$customPropertiesObj = [PSCustomObject]@{}
 		}
 
-        # prepare the exceptions info.
-        # this parses the exceptions and inner exceptions with stack traces and turns them into a format friendly for app insights.
-
-        $exceptionDetails = Convert-ExceptionToAiExceptionDetails -Exception $Exception
-        
 		# prepare the REST request body schema.
-		# NOTE: this schema represents how exceptions are sent as of the app insights .net client library v2.9.1.
+		# NOTE: this schema represents how traces are sent as of the app insights .net client library v2.9.1.
 		# newer versions of the library may change the schema over time and this may require an update to match schemas found in newer libraries.
 		
 		$bodyObject = [PSCustomObject]@{
-			'name' = "Microsoft.ApplicationInsights.$InstrumentationKey.Exception"
+			'name' = "Microsoft.ApplicationInsights.$InstrumentationKey.Trace"
 			'time' = ([System.dateTime]::UtcNow.ToString('o'))
 			'iKey' = $InstrumentationKey
 			'tags' = [PSCustomObject]@{
@@ -84,10 +89,11 @@ function Send-AppInsightsExceptionTelemetry
 				'ai.internal.sdkVersion' = 'AzurePowerShellUtilityFunctions'
 			}
 			'data' = [PSCustomObject]@{
-				'baseType' = 'ExceptionData'
+				'baseType' = 'MessageData'
 				'baseData' = [PSCustomObject]@{
 					'ver' = '2'
-					'exceptions' = $exceptionDetails
+					'message' = $Message
+                    'severityLevel' = $Severity
 					'properties' = $customPropertiesObj
 				}
 			}
@@ -97,11 +103,10 @@ function Send-AppInsightsExceptionTelemetry
 		# prepare the headers
 		# send the request
 
-		$bodyAsCompressedJson = $bodyObject | ConvertTo-JSON -Depth 20 -Compress
+		$bodyAsCompressedJson = $bodyObject | ConvertTo-JSON -Depth 10 -Compress
 		$headers = @{
 			'Content-Type' = 'application/x-json-stream';
 		}
-
 		Invoke-RestMethod -Uri $AppInsightsIngestionEndpoint -Method Post -Headers $headers -Body $bodyAsCompressedJson
     }
 }
